6cf14a2985372157e13b967b8713fe1183fb3ddc
OpusMag <90897827+OpusMag@users.noreply.github.com>
2023-09-17 17:10:25 +0200

    added code for rendering templates

--- magnusitewebserver.py
+++ magnusitewebserver.py
@@ -1,3 +1,4 @@
+#.gitignore
 from flask import Flask, Flask-login, Flask-sqlalchemy
 import os
 
@@ -6,7 +7,7 @@ PORT = 8080
 
 def make_site():
     app = flask.Flask(__name__)
-    app.config['SECRET_KEY'] = 'acALnaueifbvryhtkas'
+    app.config['SECRET_KEY'] = ' '
         
 
 @app.route('/')
@@ -14,12 +15,20 @@ def index():
     return flask.render_template('index.html')
 
 @app.route('/about')
+def about():
+    return flask.render_template('about.html')
 
 @app.route('/contact')
+def about():
+    return flask.render_template('contact.html')
 
 @app.route('/projects')
+def about():
+    return flask.render_template('projects.html')
 
 @app.route('/blog')
+def about():
+    return flask.render_template('blog.html')
 
 @app.route('/api')
 def api():
